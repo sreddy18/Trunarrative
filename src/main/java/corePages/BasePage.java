735ca7225c7a32b4172ec2a44b0e02a7e3b41276
susmitha.vinta <susmitha.vinta@dwp.gov.uk>
2019-09-22 20:22:43 +0100

    Added code to read data from excel

--- src/main/java/corePages/BasePage.java
+++ src/main/java/corePages/BasePage.java
@@ -1,5 +1,10 @@
 package corePages;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
@@ -13,6 +18,10 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import managers.FileReaderManager;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class BasePage {
@@ -146,4 +155,60 @@ public class BasePage {
         actions.sendKeys(Keys.UP).build().perform();
     }
 
+    public void readExcel(String filePath, String fileName, String sheetName) throws IOException {
+        //Create an object of File class to open xlsx file
+        File file = new File(filePath + "\\" + fileName);
+
+
+        //Create an object of FileInputStream class to read excel file
+        FileInputStream fis = new FileInputStream(file);
+
+
+        Workbook workbook = null;
+
+        //Find the file extension by splitting file name in substring  and getting only extension name
+        String fileExtensionName = fileName.substring(fileName.indexOf("."));
+
+        //Check condition if the file is xlsx file
+        if (fileExtensionName.equals(".xlsx")) {
+
+            //If it is xlsx file then create object of XSSFWorkbook class
+
+            workbook = new XSSFWorkbook(fis);
+
+        }
+        //Check condition if the file is xls file
+
+        else if (fileExtensionName.equals(".xls")) {
+            workbook = new HSSFWorkbook(fis);
+        }
+
+        //Read sheet inside the workbook by its name
+        Sheet sheet = workbook.getSheet(sheetName);
+
+        //Find number of rows in excel file
+
+        int rowCount = sheet.getLastRowNum() - sheet.getFirstRowNum();
+
+        //Create a loop over all the rows of excel file to read it
+
+        //BasePage.readExcel(filePath,"ExportExcel.xlsx","ExcelGuru99Demo");
+        for (int i = 0; i <= rowCount; i++) {
+            Row row = sheet.getRow(i);
+
+            //Create a loop to print cell values in a row
+
+            for (int j = 0; j < row.getLastCellNum(); j++) {
+                System.out.print(row.getCell(j).getStringCellValue() + "|| ");
+
+            }
+
+            // How to read data from excel in selenium web driver with java.
+            //XSSFSheet excelSheet = TestListener.readExcel("DataFiles\\testData.xlsx", "Sheet1");
+            //String msg = excelSheet.getRow(0).getCell(1).getStringCellValue();
+            // driver.findElement(By.xpath(InputFromsPage.inputMSG)).sendKeys(msg);
+        }
+
+
+    }
 }
